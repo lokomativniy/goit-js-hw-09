3ce3096025db3cce93576f897ff248a772694ce2
Dmitriy <dmitriy.sergiyenko.87@gmail.com>
2021-11-14 20:08:55 +0300

    hw9

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,16 @@
+
+const startBtn = document.querySelector(".js-start");
+const stopBtn = document.querySelector(".js-stop");
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    console.log(`I love async JS!  ${Math.random()}`);
+  }, 1000);
+});
+
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  console.log(`Interval with id ${timerId} has stopped!`);
+});
